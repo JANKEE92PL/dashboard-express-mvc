4c76ac6bbfd91867a3414ea10c7278db213f8141
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-11-15 16:18:18 +0100

    add Download TD row with file icons

--- controllers/downloadsController.js
+++ controllers/downloadsController.js
@@ -1,25 +1,36 @@
 let downloadsJson = [{
     "title": "Title from File Awesome JPG FILE",
     "subtitle": "Subtitle File Awesome JPG FILE",
+    "type": "jpg",
+    "icon": "-jpg",
     "filesize": 300,
     "unit": "mb",
     "url": "filename.jpg",
-    "online": true
+    "online": true,
+    "downloaded": true
 }, {
     "title": "Title from File Awesome PNG FILE",
     "subtitle": "Subtitle File Awesome PNG FILE",
+    "type": "png",
+    "icon": "-png",
     "filesize": 80,
     "unit": "mb",
     "url": "filename.png",
-    "online": true
+    "online": true,
+    "downloaded": false
+
 }, {
     "title": "Title from File Awesome EXE FILE",
     "subtitle": "Subtitle File Awesome EXE FILE",
+    "type": "exe",
+    "icon": "",
     "filesize": 2,
     "unit": "gb",
     "url": "filename.exe",
-    "online": false
+    "online": false,
+    "downloaded": true,
 }]
+
 
 const downloadView = (req, res) => {
     res.render("downloads", {path: req.path, data: downloadsJson});
